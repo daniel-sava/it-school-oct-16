71ca895934690e7643105910d3f8c7024a788322
Daniel Sava <dsava@gamomat.com>
2024-01-03 17:57:09 +0200

    JS Introduction

---
+++ js-introduction/script.js
@@ -0,0 +1,57 @@
+let sesiuneaCurenta = 12;
+let numarCursantiAstazi;
+
+sesiuneaCurenta = 13;
+console.log(sesiuneaCurenta);
+
+numarCursanti = 11;
+
+console.log(numarCursanti);
+
+let x = 10;
+let y = 23;
+const variabilaLuiRaul = 3;
+let result = x * y + variabilaLuiRaul * x;
+
+console.log(x);
+
+x--;
+x--;
+
+console.log(x);
+
+console.log(result);
+
+let numar1 = 44;
+let numar2 = "4 4";
+
+console.log(numar1 == numar2);
+
+console.log(true);
+console.log(false);
+
+console.log(3 - 1);
+console.log(3 + 1);
+console.log("3" - 1);
+console.log("3" + 1);
+
+let numar3 = 25;
+let numar4 = "25";
+
+console.log(numar3 === numar4);
+
+let numar5 = 123;
+let numar6 = 123;
+
+console.log(numar5 !== numar6);
+
+console.log(0.2 + 0.1 == 0.3);
+console.log(0.2 + 0.1);
+
+console.log(0.5 + 0.1 == 0.6);
+
+console.log(true - false);
+
+console.log(typeof NaN);
+
+console.log(3 > 2 >= 1);
---
+++ js-introduction/script2.js
@@ -0,0 +1,19 @@
+let text1 = "AB";
+let text2 = "AC";
+let result = text1 > text2;
+
+console.log(result);
+
+let text3 = "Numele meu este";
+let text4 = ", Daniel Sava!";
+
+let textComplet = text3 + ", " + text4 + "!";
+
+console.log(textComplet);
+
+let number = 20;
+
+number = number + 12;
+number += 12;
+number -= 20;
+number *= 5;
